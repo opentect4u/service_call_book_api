26798fa0576c32dcbaf1bc7c511fba1ab6b406a7
subhamsamanta <subham.emdee@gmail.com>
2021-06-15 17:17:22 +0530

    Change Database Connection 150621

--- Schema/db.js
+++ Schema/db.js
@@ -2,11 +2,16 @@ const mysql = require('mysql');
 
 
 const db = mysql.createPool({
+    // connectionLimit: 10,
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'support_log'
     connectionLimit: 10,
     host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'support_log'
+    user: 'ServiceCallBook',
+    password: 'ServiceCallBook@123',
+    database: 'service_call_book'
 });
 
 db.getConnection((err, connection) => {
